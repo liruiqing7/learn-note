aa24f738959bbccbb2d1d797501fd66271594314
liruiqing <liruiqing@zcool.com.cn>
2020-11-17 00:00:21 +0800

    test

---
+++ Promise/test.js
@@ -0,0 +1,17 @@
+var p1 = new Promise(function (resolve) {
+  setTimeout(function () {
+    resolve(1);
+  }, 1000);
+});
+
+p1.then(function (val) {
+  var p3 = new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(val + 1);
+    }, 1000);
+  });
+
+  return p3;
+}).then(function (val) {
+  console.log(val);
+});
