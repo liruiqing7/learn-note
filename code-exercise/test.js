5efc0429a66df82da9c1c88dcda511308f625b00
liruiqing7 <liruiqing7@163.com>
2024-06-03 15:07:01 +0800

    更新

--- code-exercise/test.js
+++ code-exercise/test.js
@@ -1,9 +1,31 @@
-Function.prototype.myCall = function (context = gloablTHis) {
-  context.fn = this;
-  console.log(this, "this");
-  const otherArgs = Array.from(this.arguments).slice(1);
-  context.fn(otherArgs);
-  let res = context.fn();
-  delete context.fn;
-  return res;
-};
+// function twoNumAdd(nums, target) {
+//   let map = new Map();
+//   for (let i = 0; i < nums.length; i++) {
+//     let num = nums[i];
+//     if(){
+
+//     }
+//   }
+//   return [];
+// }
+
+const nums = [8, 4, 3, 5, 3, 2],
+  target = 8;
+
+// console.log(twoNumAdd(nums, target));
+
+function twoNumAdd2(arr, target) {
+  if (Array.isArray(arr)) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[i] + arr[j] == target && i != j) {
+          return [arr[i], arr[j]];
+        }
+      }
+    }
+  } else {
+    return [];
+  }
+}
+
+console.log(twoNumAdd2(nums, target));
